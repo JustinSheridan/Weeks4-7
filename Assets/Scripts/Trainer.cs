4862c9559f889d3ee8e6d68ac9fe21ca2711676d
JustinSheridan <macazo@sheridancollege.ca>
2026-05-26 15:33:02 -0400

    Week4, Trainer, TurnColour Red

--- Assets/Scripts/Trainer.cs
+++ Assets/Scripts/Trainer.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Trainer : MonoBehaviour
 {
+    public SpriteRenderer creatureRenderer;
+    public Camera gameCamera;
+    public Color caughtColour;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +16,17 @@ public class Trainer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool isClicked = Mouse.current.leftButton.wasPressedThisFrame;
+
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(mousePosition);
+        worldMousePosition.z = 0f;
+
+
+        if(isClicked && creatureRenderer.bounds.Contains(worldMousePosition))
+        {
+            Debug.Log("Caught the creature!");
+            creatureRenderer.color = caughtColour;
+        }
     }
 }
