d8a80b90088bef70fd266857d58ea2403d850de8
JustinSheridan <macazo@sheridancollege.ca>
2026-05-26 16:11:36 -0400

    Week4, Uncaught Creature List

--- Assets/Scripts/Trainer.cs
+++ Assets/Scripts/Trainer.cs
@@ -1,11 +1,17 @@
+//WEEK FOUR STUFF:
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class Trainer : MonoBehaviour
 {
     public SpriteRenderer creatureRenderer;
     public Camera gameCamera;
     public Color caughtColour;
+
+    public List<SpriteRenderer> uncaughtCreatures;
+    public List<SpriteRenderer> caughtCreatures;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,11 +28,41 @@ public class Trainer : MonoBehaviour
         Vector3 worldMousePosition = gameCamera.ScreenToWorldPoint(mousePosition);
         worldMousePosition.z = 0f;
 
+        if(isClicked)
+        {
+            //caughtCreatures:
+            //for each element I want to output that to the console
+
+            //FIRST SECTION: Define and set the value of the iterator
+            //SECOND SECTION: Define how long the loop will run
+            //THIRD SECTION: Increase the value of i each time it loops
+
+            for(int i = 0; i < caughtCreatures.Count; i++)
+            {
+                Debug.Log(caughtCreatures[i]);
+            }
+
+
+            if(caughtCreatures.Count > 0)
+            {
+                Debug.Log(caughtCreatures[0]);
+            }
+            
+        }
+
 
         if(isClicked && creatureRenderer.bounds.Contains(worldMousePosition))
         {
-            Debug.Log("Caught the creature!");
             creatureRenderer.color = caughtColour;
+
+            bool isCreatureCaught = caughtCreatures.Contains(creatureRenderer);
+            //Debug.Log("Is creature caught["+isCreatureCaught.ToString()+"]");
+            if(!isCreatureCaught)
+            {
+                caughtCreatures.Add(creatureRenderer);
+            }
+            
+            uncaughtCreatures.Remove(creatureRenderer);
         }
     }
 }
